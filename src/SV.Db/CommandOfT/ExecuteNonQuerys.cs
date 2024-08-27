d42f5d313a4b8b03f2a361d24b5e20c226eb74fc
Victor Qu <Victor.X.Qu@newegg.com>
2024-08-27 13:34:18 +0800

    ExecuteNonQuerys

--- src/SV.Db/CommandOfT/ExecuteNonQuerys.cs
+++ src/SV.Db/CommandOfT/ExecuteNonQuerys.cs
@@ -1,11 +1,24 @@
 ﻿using System.Data;
 using System.Data.Common;
-using System.Threading;
 
 namespace SV.Db
 {
     public static partial class CommandExtensions
     {
+        public static int ExecuteNonQuerys<T>(this DbConnection connection, string sql, List<T> args, int batchSize = 100, CommandType commandType = CommandType.Text)
+        {
+            if (args == null || args.Count == 0) return 0;
+            if (args.Count == 1) return connection.ExecuteNonQuery(sql, args[0], commandType);
+            return ExecuteNonQuerys<T>(connection, sql, args, batchSize, commandType);
+        }
+
+        public static int ExecuteNonQuerys<T>(this DbConnection connection, string sql, T[] args, int batchSize = 100, CommandType commandType = CommandType.Text)
+        {
+            if (args == null || args.Length == 0) return 0;
+            if (args.Length == 1) return connection.ExecuteNonQuery(sql, args[0], commandType);
+            return ExecuteNonQuerys<T>(connection, sql, args, batchSize, commandType);
+        }
+
         public static int ExecuteNonQuerys<T>(this DbConnection connection, string sql, IEnumerable<T> args, int batchSize = 100, CommandType commandType = CommandType.Text)
         {
             try
@@ -34,6 +47,20 @@ namespace SV.Db
             {
                 connection.Close();
             }
+        }
+
+        public static Task<int> ExecuteNonQuerysAsync<T>(this DbConnection connection, string sql, List<T> args, int batchSize = 100, CancellationToken cancellationToken = default, CommandType commandType = CommandType.Text)
+        {
+            if (args == null || args.Count == 0) return Task.FromResult(0);
+            if (args.Count == 1) return connection.ExecuteNonQueryAsync(sql, args[0], cancellationToken, commandType);
+            return ExecuteNonQuerysAsync<T>(connection, sql, args, batchSize, cancellationToken, commandType);
+        }
+
+        public static Task<int> ExecuteNonQuerysAsync<T>(this DbConnection connection, string sql, T[] args, int batchSize = 100, CancellationToken cancellationToken = default, CommandType commandType = CommandType.Text)
+        {
+            if (args == null || args.Length == 0) return Task.FromResult(0);
+            if (args.Length == 1) return connection.ExecuteNonQueryAsync(sql, args[0], cancellationToken, commandType);
+            return ExecuteNonQuerysAsync<T>(connection, sql, args, batchSize, cancellationToken, commandType);
         }
 
         public static async Task<int> ExecuteNonQuerysAsync<T>(this DbConnection connection, string sql, IEnumerable<T> args, int batchSize = 100, CancellationToken cancellationToken = default, CommandType commandType = CommandType.Text)
